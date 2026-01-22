9a26ecb28d2f6b5637afa4da5233b928092b80d9
sidsun1 <siddharthsundar2@gmail.com>
2026-01-22 14:57:20 -0800

    TitlePage component/css restructure

--- gamified-mst/app/page.tsx
+++ gamified-mst/app/page.tsx
@@ -1,44 +1,7 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
-import './title.css';
+import TitlePage from './components/TitlePage/TitlePage';
 
 export default function Page() {
-  const router = useRouter();
-
-  return (
-    <div className="scene">
-      {/* Sun */}
-      <div className="sun" />
-
-      {/* Clouds */}
-      <div className="cloud cloud1" />
-      <div className="cloud cloud2" />
-
-      {/* Title */}
-      <div className="titleBox">
-        <div className="titleText">THE MST</div>
-      </div>
-
-      {/* Start Button */}
-      <button
-        className="startButton"
-        onClick={() => router.push('/task?mode=Flatx2')}
-      >
-        START
-      </button>
-
-      {/* Ground */}
-      <div className="ground">
-        {/* Tree */}
-        <div className="tree">
-          <div className="leaves" />
-          <div className="trunk" />
-        </div>
-
-        {/* Anteater placeholder */}
-        <div className="anteater" />
-      </div>
-    </div>
-  );
+    return <TitlePage />
 }
