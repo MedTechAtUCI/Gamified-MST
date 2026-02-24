62985ffe29cffc063375c8105050bde4bdb73dc2
LoganMiff <logan.mifflin@gmail.com>
2026-02-23 20:38:21 -0800

    Fixed failing deployment from useSearchParams

--- gamified-mst/app/task/page.tsx
+++ gamified-mst/app/task/page.tsx
@@ -1,15 +1,24 @@
 'use client';
 
 import { useSearchParams } from 'next/navigation';
+import { Suspense } from 'react'
 import TaskPage from '@/app/components/TaskPage/TaskPage';
 import { TaskType } from '@/app/types/mst';
 
-export default function TaskRoutePage() {
+function TaskContent() {
   const searchParams = useSearchParams();
-
+  
   // Read task type from URL (?mode=Flatx2)
   const taskType =
     (searchParams.get('mode') as TaskType) ?? 'Flatx2';
 
   return <TaskPage taskType={taskType} />;
+}
+
+export default function TaskRoutePage() {
+    return (
+      <Suspense fallback={<div>Loading Task...</div>}>
+        <TaskContent />
+      </Suspense>
+    );
 }
