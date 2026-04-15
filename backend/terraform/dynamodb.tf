db31a4d82febaba1745f7ce23bb6d31a1657ee86
sidsun1 <siddharthsundar2@gmail.com>
2026-04-15 16:36:44 -0700

    Consolidated AWS svcs w/ user state

--- backend/terraform/dynamodb.tf
+++ backend/terraform/dynamodb.tf
@@ -17,7 +17,29 @@ resource "aws_dynamodb_table" "metrics_table" {
   }
 }
 
-# Lambda reference
+resource "aws_dynamodb_table" "user_state_table" {
+  name         = "user-state-table"
+  billing_mode = "PAY_PER_REQUEST"
+
+  hash_key  = "userId"
+  range_key = "gameType"
+
+  attribute {
+    name = "userId"
+    type = "S"
+  }
+
+  attribute {
+    name = "gameType"
+    type = "S"
+  }
+}
+
+# Lambda references
 output "metrics_table_name" {
   value = aws_dynamodb_table.metrics_table.name
+}
+
+output "user_state_table_name" {
+  value = aws_dynamodb_table.user_state_table.name
 }
